4bda19101601a4a759ee3a71fd307a3d188549f4
Klesley Silva de Oliveira <klesley.oliveira@hotmail.com>
2026-05-27 16:31:20 -0300

    Get Address by Cep implementation

---
+++ src/main/java/one/digitalinnovation/gof/controller/AddressController.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.gof.model.dto.ViaCepRequestDTO;
+import one.digitalinnovation.gof.service.ViaCepService;
+
+@RestController
+@RequestMapping(value = "/address")
+public class AddressController {
+	
+	@Autowired
+	private ViaCepService service;
+	
+	@GetMapping(value = "/{cep}")
+	public ResponseEntity<ViaCepRequestDTO> getAddressByCep(@PathVariable String cep){
+		return ResponseEntity.ok(service.getCep(cep));
+	}
+}
